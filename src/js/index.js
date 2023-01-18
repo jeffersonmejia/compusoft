54f5329cae7865c65ce982ac764fbb26dd5c0d67
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-01-17 23:30:39 -0500

    add burger modal list

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-//remove this after make pull
-console.log("hi");
+const d = document,
+	$navbarList = d.querySelector(".navbar ul"),
+	$burgerList = d.querySelector(".burger-list"),
+	$navbar = d.getElementById("navbar-template").content;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+	let $navbarClone = $navbar.cloneNode(true);
+	$navbarList.appendChild($navbar);
+	$burgerList.appendChild($navbarClone);
+});
+d.addEventListener("click", (e) => {
+	if (e.target.matches(".burger")) {
+		$burgerList.classList.toggle("opacity-hidden");
+	}
+});
