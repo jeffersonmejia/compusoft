675853bfb573f80b55550fc2301ec0498e534b5f
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-01-18 00:13:01 -0500

    update form contact

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 const d = document,
 	$navbarList = d.querySelector(".navbar ul"),
 	$burgerList = d.querySelector(".burger-list"),
-	$navbar = d.getElementById("navbar-template").content;
+	$navbar = d.getElementById("navbar-template").content,
+	$contact = d.querySelector(".contact-us-container");
 
 d.addEventListener("DOMContentLoaded", (e) => {
 	let $navbarClone = $navbar.cloneNode(true);
@@ -11,5 +12,14 @@ d.addEventListener("DOMContentLoaded", (e) => {
 d.addEventListener("click", (e) => {
 	if (e.target.matches(".burger")) {
 		$burgerList.classList.toggle("opacity-hidden");
+	}
+	if (e.target.matches(".link-contact")) {
+		$contact.classList.remove("opacity-hidden");
+	}
+	if (e.target.matches(".navbar a") || e.target.matches(".burger-list a")) {
+		e.preventDefault();
+	}
+	if (e.target.matches(".btn-back")) {
+		$contact.classList.add("opacity-hidden");
 	}
 });
