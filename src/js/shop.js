7064ea5b4e4f11060b0ad2f616a52fe212bafa57
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-02-05 18:41:37 -0500

    update styles

--- src/js/shop.js
+++ src/js/shop.js
@@ -1,33 +1,38 @@
-$(document).ready(function(){
-    $('.category_list .category_item[category="all"]').addClass('ct_item-active');
+$(document).ready(function () {
+	$('.category_list .category_item[category="all"]').addClass("ct_item-active");
 
-    $('.category_item').click(function(){
-        var catProduct = $(this).attr('category');
-        console.log(catProduct);
+	$(".category_item").click(function () {
+		var catProduct = $(this).attr("category");
+		console.log(catProduct);
 
-        $('.category_item').removeClass('ct_item-active');
-        $(this).addClass('ct_item-active');
-        
-        $('.product-item').css('transform', 'scale(0)');
-        function hideProduct(){
-           $('.product-item').hide();
-        } setTimeout(hideProduct,400);
+		$(".category_item").removeClass("ct_item-active");
+		$(this).addClass("ct_item-active");
 
-        function showProduct(){
-            $('.product-item[category="'+catProduct+'"]').show();
-            $('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
-        } setTimeout(showProduct,400);
-        
-    });
-    $('.category_item[category="all"]').click(function(){
-        function showAll(){
-            $('.product-item').show();
-            $('.product-item').css('transform', 'scale(1)');
-        }setTimeout(showAll,400);
-        
-    });
+		$(".product-item").css("transform", "scale(0)");
+		function hideProduct() {
+			$(".product-item").hide();
+		}
+		setTimeout(hideProduct, 400);
+
+		function showProduct() {
+			$('.product-item[category="' + catProduct + '"]').show();
+			$('.product-item[category="' + catProduct + '"]').css("transform", "scale(1)");
+		}
+		setTimeout(showProduct, 400);
+	});
+	$('.category_item[category="all"]').click(function () {
+		function showAll() {
+			$(".product-item").show();
+			$(".product-item").css("transform", "scale(1)");
+		}
+		setTimeout(showAll, 400);
+	});
 });
 
+const d = document,
+	$modalLoader = d.querySelector(".modal-loader");
 
-
-
+function stopLoader() {
+	setTimeout(() => $modalLoader.classList.add("hidden"), 1000);
+}
+d.addEventListener("DOMContentLoaded", (e) => stopLoader());
